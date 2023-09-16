8db8e1cb9fd3759b6d2554cf99be7f2411c133de
Ömer Kıraç <okrac54@hotmail.com>
2023-09-16 12:40:42 +0300

    first commit

---
+++ src/main/java/com/kafein/intern/postinger_api_gateway/PostingerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kafein.intern.postinger_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostingerApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostingerApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafein/intern/postinger_api_gateway/PostingerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafein.intern.postinger_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingerApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
